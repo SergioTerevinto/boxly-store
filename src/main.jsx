4ecf42611fb920f274ac26ea96a33b9f5a6246e1
Sergio Terevinto <terevintosergio@gmail.com>
2025-04-22 20:51:35 -0300

    ULTIMA ENTREGA

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { CartProvider } from './context/CartContext' // importás el contexto
+import { CartProvider } from './context/CartContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
